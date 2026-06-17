7d11e9dbf42e49b19d4e746fa039ba59f6393d6a
Mounishadevi <118105652+Mounishadevi@users.noreply.github.com>
2026-06-17 16:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-MODEL_NAME = "models/gemini-2.0-flash"
+MODEL_NAME = "models/gemini-2.5-flash"
 
 # ==========================
 # CHAT HISTORY
